f892c4b97513b9ff6d590a1fd4f382348a405e17
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2025-07-28 21:30:03 +0600

    Add files via upload

---
+++ pr6 phone book using json file.py
@@ -0,0 +1,155 @@
+import json
+
+import os
+
+contacts = {
+
+}
+
+FILE_NAME = 'contacts.json'
+
+def load_contact():
+
+    if os.path.exists(FILE_NAME):
+
+        with open(FILE_NAME, 'r') as f:
+
+            return json.load(f)
+
+    else:
+
+        return {}
+
+def save(contacts):
+
+    with open(FILE_NAME, 'w') as f:
+
+        json.dump(contacts, f, indent = 4)
+
+print(f'{'Phone Book':>{15}}')
+
+print(20*'=')
+
+contacts = load_contact()
+
+def menu():
+
+    print('\nEnter the operation you want to perform:-')
+
+    print(42*'_')
+
+    print('1. Add a contact.')
+
+    print('2. View contacts.')
+
+    print('3. Search contacts')
+
+    print('4. Delete a contact.')
+
+    print('Exit the program')
+
+def add ():
+
+    #contacts = load_contact()
+
+    name = input('Enter the contacts name :- ').title()
+
+    phone = input('Enter the phone number :- ')
+
+    email = input('Enter the email :- ').lower()
+
+    contacts[name] = {
+
+        'phone': phone,
+
+        'email': email
+
+    }
+
+    save(contacts)
+
+    print('Contact has been added successfully. ✅')
+
+def view():
+
+    #contacts = load_contact()
+
+    if not contacts:
+
+        print('\nThere is no contact available. ❌')
+
+    else:
+
+        for name, info in contacts.items():
+
+            print('\nName: ', name)
+
+            print('Phone :', info['phone'])
+
+            print('Email :', info['email'])
+
+            print(28*'-')
+
+def search ():
+
+    name = input('Enter tha name of the contact :- ').title()
+
+    if name in contacts:
+
+        info = contacts[name]
+
+        print(f'\nFound the contact for {name}')
+
+        print(f'Phone : {info['phone']}')
+
+        print(f'Phone : {info['email']}')
+    else:
+
+        print('\nThe contact can\'t be found. ❌')
+
+def delete ():
+
+    name = input('Enter the name of the contact to delete :- ')
+
+    if name in contacts:
+
+        del contacts[name]
+
+        save(contacts)
+
+    else:
+
+        print('\nThe contact can\'t be found. ❌')
+
+while True:
+
+    menu()
+
+    try:
+        operation = int(input('Enter the operation you want to perform :- '))
+
+    except ValueError:
+
+        print('Oops invalid input please enter a number between 1-2')
+        continue
+
+    if operation == 1:
+
+        add()
+
+    elif operation == 2:
+
+        view()
+
+    elif operation == 3:
+
+        search()
+
+    elif operation == 4:
+
+        delete()
+
+    elif operation == 5:
+
+        print('Thank you for using this program. ❤️')
+        break
